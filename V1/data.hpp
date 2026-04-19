a0824cf39730f9d986b4102b6d05e5777f1ba8e8
林布帆 <3627665738@qq.com>
2026-04-19 15:07:36 +0800

    read内数据迁移至data,read修改后缀名

--- V1/data.hpp
+++ V1/data.hpp
@@ -11,3 +11,8 @@ char asm_name[_MAX_FNAME];
 size_t size_of_file_content;
 stack<char> stk;      // 用来配对大小括号
 string code_function; // 暂时不考虑实现
+string file_content;
+vector<string> file_content_split;
+string read(const char filename[]) {}
+string code_data; // 存储data区的代码
+string code_code; // 存储code区的代码
---
+++ V1/read.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef READ_HPP
+#define READ_HPP
+
+#endif // READ_HPP
+#include<bits/stdc++.h>
+using namespace std;
